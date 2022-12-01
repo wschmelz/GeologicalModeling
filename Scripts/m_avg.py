95d3632c22e73cd80eb0ab1c4c4cf32e44dea983
W. John Schmelz <wjs107@eps.rutgers.edu>
2022-11-30 22:40:27 -0500

    Add files via upload

--- Scripts/m_avg.py
+++ Scripts/m_avg.py
@@ -2,15 +2,15 @@
 
 import numpy
 
-def m_avg(x,y,span):
-	output = numpy.zeros(len(x))*numpy.nan
-	for n in range(0,len(x)):
+def m_avg(x_new,x_orig,y_orig,span):
+	output = numpy.zeros(len(x_new))*numpy.nan
+	for n in range(0,len(x_new)):
 		#set interpolation value
-		t_interp = x[n]
+		t_interp = x_new[n]
 		
 		#find all data values where data is within specified "window" relative to interpolation value
-		w1 = numpy.where(numpy.absolute(x - t_interp) < span)[0]
+		w1 = numpy.where(numpy.absolute(x_orig - t_interp) < span)[0]
 		
-		output[n] = numpy.mean(y[w1])
+		output[n] = numpy.mean(y_orig[w1])
 	
 	return output
